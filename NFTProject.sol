2cff088d3865a44d4295b19b3ce3a200fe9e94ea
Paul Len <paulstanlen@gmail.com>
2022-10-31 20:34:39 -0400

    Homework 9

---
+++ NFTProject.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"; 
+import "@openzeppelin/contracts/access/ERC721.sol";
+
+contract NFTProject is ERC721 {
+
+    function constructor(string _name, string _symbol){
+        
+    }
+}
